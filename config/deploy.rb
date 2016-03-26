0bfcbf3083faae33499cb02ccba15ce462f77a85
Alexander Graefe <alex@codemo.de>
2016-03-26 12:08:54 +0100

    Guess what...

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,7 +35,7 @@ set :deploy_to, '/opt/www/perilous-wilds.geekwire.net'
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
 # Default value for linked_dirs is []
-set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'vendor/bundle')
+set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'vendor/bundle', 'tmp/puma')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
