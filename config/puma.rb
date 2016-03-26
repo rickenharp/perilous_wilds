bab301ffa42c70fe7f821871949dfbaa2283dfd1
Alexander Gräfe <alexander.graefe@googlemail.com>
2016-03-26 12:08:54 +0100

    Guess what...

--- config/puma.rb
+++ config/puma.rb
@@ -1,6 +1,8 @@
 workers 2
 threads 1, 6
+environment 'production'
 bind 'unix:////tmp/puma.perilous-wilds.sock'
 directory '/opt/www/perilous-wilds.geekwire.net/current'
-pidfile "/opt/www/perilous-wilds.geekwire.net/current/tmp/pids/puma.pid"
-state_path "/opt/www/perilous-wilds.geekwire.net/tmp/puma/state"
+pidfile "/opt/www/perilous-wilds.geekwire.net/current/tmp/puma/pid"
+state_path "/opt/www/perilous-wilds.geekwire.net/current/tmp/puma/state"
+activate_control_app
