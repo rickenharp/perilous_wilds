654cb39c2e6996245ce44459735913e3a23873a8
Alexander Gräfe <alexander.graefe@googlemail.com>
2016-04-19 13:18:53 +0200

    Add Lair

---
+++ models/discovery/natural_feature/lair.rb
@@ -0,0 +1,13 @@
+class Discovery < RandomElement
+  class NaturalFeature < RandomElement
+    class Lair < RandomElement
+      OPTIONS = RangedHash.new(
+        (1..3) => 'burrow',
+        (4..7) => 'cave/tunnel',
+        (8..9) => 'nest/aerie',
+        (10..10) => 'hive',
+        (11..12) => 'ruins' # TODO: Structure
+      ).freeze
+    end
+  end
+end
