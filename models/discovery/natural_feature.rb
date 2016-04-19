aae12a7ed7ab8f1415a3317d1ddd1de34fea4e71
Alexander Gräfe <alexander.graefe@googlemail.com>
2016-04-19 12:10:39 +0200

    Add NaturalFeature

---
+++ models/discovery/natural_feature.rb
@@ -0,0 +1,40 @@
+require 'models/random_element'
+
+class Discovery < RandomElement
+  class NaturalFeature < RandomElement
+    OPTIONS = RangedHash.new(
+      (1..2) => '<%= lair %>',
+      (3..4) => '<%= obstacle %>',
+      (5..7) => '<%= terrain_change %>',
+      (8..9) => '<%= water_feature %>',
+      (10..11) => '<%= landmark %>',
+      (12..12) => '<%= resource %>'
+    ).freeze
+
+    private
+
+    def lair
+      'Lair'
+    end
+
+    def obstacle
+      'Obstacle'
+    end
+
+    def terrain_change
+      'Terrain Change'
+    end
+
+    def water_feature
+      'Water Feature'
+    end
+
+    def landmark
+      'Landmark'
+    end
+
+    def resource
+      'Resource'
+    end
+  end
+end
