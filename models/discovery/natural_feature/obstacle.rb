a14dc849e0561aece76e998fef53953cfdc4c7c5
Alexander Gräfe <alexander.graefe@googlemail.com>
2016-04-19 13:25:47 +0200

    Add Obstacle

---
+++ models/discovery/natural_feature/obstacle.rb
@@ -0,0 +1,12 @@
+class Discovery < RandomElement
+  class NaturalFeature < RandomElement
+    class Obstacle < RandomElement
+      OPTIONS = RangedHash.new(
+        (1..5) => 'difficult ground (specific to terrain)',
+        (6..8) => 'cliff/crevasse/chasm',
+        (9..10) => 'ravine/gorge',
+        (11..12) => 'hive' # TODO: Oddity
+      ).freeze
+    end
+  end
+end
