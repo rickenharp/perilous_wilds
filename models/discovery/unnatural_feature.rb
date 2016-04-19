37b9bd1fc5d0419118538e41ff14d9236ae7ce58
Alexander Gräfe <alexander.graefe@googlemail.com>
2016-04-19 11:51:47 +0200

    Refactor UnnaturalFeature

--- models/discovery/unnatural_feature.rb
+++ models/discovery/unnatural_feature.rb
@@ -3,9 +3,23 @@ require 'models/random_element'
 class Discovery < RandomElement
   class UnnaturalFeature < RandomElement
     OPTIONS = RangedHash.new(
-      (1..9) => 'Arcane',
-      (10..11) => 'Planar',
-      (12..12) => 'Divine'
+      (1..9) => '<%= arcane %>',
+      (10..11) => '<%= planar %>',
+      (12..12) => '<%= divine %>'
     ).freeze
+
+    private
+
+    def arcane
+      'Arcane'
+    end
+
+    def planar
+      'Planar'
+    end
+
+    def divine
+      'Divine'
+    end
   end
 end
