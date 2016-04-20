ead61dee6ac6c1b94f68562c916ccafbaf83715a
Alexander Graefe <alex@codemo.de>
2016-04-20 20:39:02 +0200

    Add Feature

---
+++ models/details/feature.rb
@@ -0,0 +1,40 @@
+module Details
+  class Feature < Table
+    OPTIONS = RangedHash.new(
+      (1..1) => 'heavily armored',
+      (2..3) => 'winged/flying',
+      (4..4) => 'multiple heads/headless',
+      (5..5) => 'many eyes/one eye',
+      (6..6) => 'many limbs/tails',
+      (7..7) => 'tentacles/tendrils',
+      (8..8) => '<%= aspect %>',
+      (9..9) => '<%= element %>',
+      (10..10) => '<%= magic_type %>',
+      (11..11) => '<%= oddity %>',
+      (12..12) => '<%= roll_twice %>'
+    ).freeze
+
+    def aspect
+      @aspect ||= "#{Details::Aspect.new}"
+    end
+
+    def element
+      @element ||= "#{Details::Element.new}"
+    end
+
+    def magic_type
+      @magic_type ||= "#{Details::MagicType.new}"
+    end
+
+    def oddity
+      @oddity ||= "#{Details::Oddity.new}"
+    end
+
+    def roll_twice
+      new_range = Range.new(1, OPTIONS.max - 1)
+      @both ||= Array.new(2) { Feature.new(rand(new_range)) }
+      self.sub_table = @both.collect(&:sub_table).uniq.first
+      @both.join(' & ')
+    end
+  end
+end
