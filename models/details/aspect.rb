2dbf8418ce8b9703ae4bfa0f9b3b6e349bb1a21d
Alexander Gräfe <alexander.graefe@googlemail.com>
2016-04-20 11:20:21 +0200

    Add Aspect detail to Divine

---
+++ models/details/aspect.rb
@@ -0,0 +1,34 @@
+module Details
+  class Aspect < Table
+    OPTIONS = RangedHash.new(
+      [
+        'power/strength',
+        'trickery/dexterity',
+        'time/constitution',
+        'knowledge/intelligence',
+        'nature/wisdom',
+        'culture/charisma',
+        'war/lies/discord',
+        'peace/truth/balance',
+        'hate/envy',
+        'love/admiration',
+        '<%= element %>',
+        '<%= roll_twice %>'
+      ]
+    ).freeze
+
+    private
+
+    def element
+      self.sub_table = Details::Element.new
+      'Element'
+    end
+
+    def roll_twice
+      new_range = Range.new(1, OPTIONS.max - 1)
+      @both ||= Array.new(2) { Aspect.new(rand(new_range)) }
+      self.sub_table = @both.collect(&:sub_table).uniq.first
+      @both.join(' & ')
+    end
+  end
+end
