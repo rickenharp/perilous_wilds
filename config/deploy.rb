1f4a9ee994d68c51ee6d735019a5a3b7543c5129
Alexander Graefe <alex@codemo.de>
2016-04-20 16:52:44 +0200

    Commit fixes

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.4.0'
 
 set :application, 'perilous-wilds'
-set :repo_url, 'ssh://git@gitlab.geekwire.net:2288/rickenharp/perilous_wilds.git'
+set :repo_url, 'ssh://git@gitlab.geekwire.net:22/rickenharp/perilous_wilds.git'
 
 set :rbenv_type, :user # or :system, depends on your rbenv setup
 set :rbenv_ruby, '2.3.0'
@@ -11,7 +11,6 @@ set :bundle_jobs, 4
 
 set :ssh_options, {
   forward_agent: true,
-  port: 2288
 }
 
 # Default branch is :master
