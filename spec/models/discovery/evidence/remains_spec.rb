c1c44368a9bf9e45f12d4fe60ed7d6dcc3d2e0c0
Alexander Graefe <alexander.graefe@extern.polizei.niedersachsen.de>
2016-04-21 14:37:05 +0200

    Add Visibility to Remains

--- spec/models/discovery/evidence/remains_spec.rb
+++ spec/models/discovery/evidence/remains_spec.rb
@@ -7,4 +7,9 @@ RSpec.describe Discovery::Evidence::Remains do
     expect(subject.details).to include('Age')
     expect(subject.details['Age']).to be_a(Details::Age)
   end
+
+  it 'has Visibility detail' do
+    expect(subject.details).to include('Visibility')
+    expect(subject.details['Visibility']).to be_a(Details::Visibility)
+  end
 end
