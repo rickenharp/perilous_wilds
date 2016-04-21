1fbb3a572018b140f8a2236c6b7f18c29141d431
Alexander Gräfe <alexander.graefe@googlemail.com>
2016-04-21 14:31:17 +0200

    Add Age to Tracks

--- models/discovery/evidence/tracks.rb
+++ models/discovery/evidence/tracks.rb
@@ -10,6 +10,7 @@ class Discovery < Table
       ).freeze
 
       def add_details
+        add_detail('Age', Details::Age.new)
         creature = Creature.new
         add_detail('Creature responsible', creature.elements.join(' &rarr; '))
       end
