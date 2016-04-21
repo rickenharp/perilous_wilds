e2213e88d41e551d40b1e5372d5f1d015e8e3457
Alexander Gräfe <alexander.graefe@googlemail.com>
2016-04-21 14:35:32 +0200

    Add Age to Remains

--- spec/models/discovery/evidence/remains_spec.rb
+++ spec/models/discovery/evidence/remains_spec.rb
@@ -2,4 +2,9 @@ require 'roda_helper'
 
 RSpec.describe Discovery::Evidence::Remains do
   it_behaves_like 'a random element'
+
+  it 'has Age detail' do
+    expect(subject.details).to include('Age')
+    expect(subject.details['Age']).to be_a(Details::Age)
+  end
 end
