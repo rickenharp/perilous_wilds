a639ff16ebe54c8fe2ab43849a0a1977f7119ce3
Alexander Graefe <alex@alexandergraefe.de>
2018-07-19 09:57:43 +0200

    Switch to sassc

--- perilous_wilds.rb
+++ perilous_wilds.rb
@@ -1,9 +1,11 @@
 $LOAD_PATH << File.expand_path('.')
 
 require 'roda'
+require 'tilt/sassc'
 require 'tilt/erb'
 require 'models'
 require 'base58'
+
 
 class PerilousWilds < Roda
   plugin :render
