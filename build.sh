1810b9c9799e104beb16a184c03c0af95b2515cd
Alexander Gräfe <alexander.graefe@googlemail.com>
2020-10-26 10:17:02 +0100

    Update ruby to 2.7.2

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 git rev-parse HEAD > REVISION
-docker build --build-arg bundle_without="test development" -t perilous_wilds .
+DOCKER_BUILDKIT=1 docker build --build-arg bundle_without="test development" -t perilous_wilds .
 rm REVISION
